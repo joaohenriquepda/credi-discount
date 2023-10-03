89f63addda243bf81aa9d02c1ab5bd314df810b7
João Henrique <joaohenrique.p.almeida@gmail.com>
2023-10-03 13:53:21 -0300

    Add debug logging level in development environment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
     def show
+        Rails.logger.info "List all users with paginate"
         @user = User.includes(:address, :contacts).find(params[:id])
     end
 end
